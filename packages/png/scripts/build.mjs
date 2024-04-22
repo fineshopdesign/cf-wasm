8e150fc2debdc690311035c36e843f6c1d5d9b17
Team Fineshop Design <inventordev480@gmail.com>
2024-04-22 17:11:29 +0530

    refactor: improved build scripts

--- packages/png/scripts/build.mjs
+++ packages/png/scripts/build.mjs
@@ -1,8 +1,10 @@
-import childProcess from "node:child_process";
+import cp from "node:child_process";
 import path from "node:path";
 import fs from "node:fs";
 
 import buildConfig from "./config.mjs";
+
+const startedOn = Date.now();
 
 // Clean out dir
 console.log("⚡ Cleaning dist dir...");
@@ -53,10 +55,21 @@ fs.writeFileSync(jsOutFile, modifiedScript, "utf-8");
 
 // Build ESM
 console.log("⚡ Building ESM...");
-childProcess.execSync("npx tsc");
-childProcess.execSync("npx copyfiles -u 1 ./src/lib/*.wasm ./dist/esm/");
+cp.execSync(
+	["pnpm tsc", "pnpm copyfiles -u 1 ./src/lib/*.wasm ./dist/esm/"].join(" && "),
+	{ stdio: "inherit" }
+);
 
 // Build CommonJS
-console.log("⚡ Building CommonJS...");
-childProcess.execSync("npx tsc -p ./tsconfig.cjs.json");
-childProcess.execSync("npx copyfiles -u 1 ./src/lib/*.wasm ./dist/cjs/");
+console.log("⚡ Building CJS...");
+cp.execSync(
+	[
+		"pnpm tsc -p ./tsconfig.cjs.json",
+		"pnpm copyfiles -u 1 ./src/lib/*.wasm ./dist/cjs/"
+	].join(" && "),
+	{ stdio: "inherit" }
+);
+
+const finishedOn = Date.now();
+
+console.log(`⚡ Took ${((finishedOn - startedOn) / 1000).toFixed(2)} s`);
