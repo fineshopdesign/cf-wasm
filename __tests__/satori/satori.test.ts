058910e62cfb65e0796eba0be383a56d883ff6cb
Team Fineshop Design <inventordev480@gmail.com>
2024-04-28 23:00:53 +0530

    refactor: separate directory (./dist/dts) for .d.ts files to minimize bundle size [bump]

--- __tests__/satori/satori.test.ts
+++ __tests__/satori/satori.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "vitest";
-import * as satori from "../dist/cjs/node";
+import * as satori from "@cf-wasm/satori/node";
 
 test("satori.YOGA_MODULE should be an instance of WebAssembly.Module", () => {
 	expect(satori.YOGA_MODULE).instanceOf(WebAssembly.Module);
--- packages/png/scripts/build.ts
+++ packages/png/scripts/build.ts
@@ -1,13 +1,14 @@
 import cp from "node:child_process";
 import path from "node:path";
-import fs from "node:fs";
+import fs from "fs-extra";
+import clc from "console-log-colors";
 
-import buildConfig from "./config.mjs";
+import buildConfig from "./config";
 
 const startedOn = Date.now();
 
 // Clean out dir
-console.log("⚡ Cleaning dist dir...");
+console.log("⚡ Cleaning dist directory...");
 fs.rmSync("dist", { recursive: true, force: true });
 
 // Delete unnecessary files from output
@@ -54,14 +55,14 @@ const modifiedScript = originalScript
 fs.writeFileSync(jsOutFile, modifiedScript, "utf-8");
 
 // Build ESM
-console.log("⚡ Building ESM...");
+console.log("⚡ Building ESM version...");
 cp.execSync(
 	["pnpm tsc", "pnpm copyfiles -u 1 ./src/lib/*.wasm ./dist/esm/"].join(" && "),
 	{ stdio: "inherit" }
 );
 
 // Build CommonJS
-console.log("⚡ Building CJS...");
+console.log("⚡ Building CJS version...");
 cp.execSync(
 	[
 		"pnpm tsc -p ./tsconfig.cjs.json",
@@ -70,6 +71,19 @@ cp.execSync(
 	{ stdio: "inherit" }
 );
 
+// Build Types
+console.log("⚡ Building Types...");
+cp.execSync("pnpm tsc -p ./tsconfig.dts.json", { stdio: "inherit" });
+
+// Write package.json
+console.log("⚡ Writing package.json files...");
+const cjsPackageJSON = { type: "commonjs" };
+
+fs.writeJSONSync("dist/cjs/package.json", cjsPackageJSON);
+fs.writeJSONSync("dist/dts/package.json", cjsPackageJSON);
+
 const finishedOn = Date.now();
 
-console.log(`⚡ Took ${((finishedOn - startedOn) / 1000).toFixed(2)} s`);
+console.log(
+	clc.green(`⚡ Took ${((finishedOn - startedOn) / 1000).toFixed(2)} s`)
+);
