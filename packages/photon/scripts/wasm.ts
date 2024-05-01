25c5a143a04f408dcc81e595ddc01cd65b9b07e6
Fineshop Design <fineshopdesign@gmail.com>
2024-05-01 14:56:28 +0530

    fix: use esbuild for building modules to fix errors such as `ERR_MODULE_NOT_FOUND`, `ERR_UNSUPPORTED_DIR_IMPORT`, etc. [bump]

--- packages/photon/scripts/wasm.ts
+++ packages/photon/scripts/wasm.ts
@@ -1,5 +1,6 @@
 import cp from "node:child_process";
 import path from "node:path";
+import fs from "fs-extra";
 
 import buildConfig from "./config";
 
@@ -10,3 +11,46 @@ cp.execSync(
 		stdio: "inherit"
 	}
 );
+
+// Delete unnecessary files from output
+[
+	"package.json",
+	"LICENSE",
+	"LICENSE.md",
+	"README",
+	"README.md",
+	".gitignore"
+].forEach((file) =>
+	fs.rmSync(path.join(buildConfig.wasm.out, file), { force: true })
+);
+
+// Make changes to out js to make it compatible
+// for different environments
+const jsOutFile = path.join(
+	buildConfig.wasm.out,
+	`${buildConfig.wasm.name}.js`
+);
+
+const originalScript = fs.readFileSync(jsOutFile, "utf-8");
+
+const modifiedScript = originalScript
+	.replace(
+		`if (!(module instanceof WebAssembly.Module)) {
+        module = new WebAssembly.Module(module);
+    }`,
+		`//! Needed to remove these lines in order to make it work on next.js
+    // if (!(module instanceof WebAssembly.Module)) {
+    //     module = new WebAssembly.Module(module);
+    // }`
+	)
+	.replace(
+		`if (typeof input === 'undefined') {
+        input = new URL('${buildConfig.wasm.name}_bg.wasm', import.meta.url);
+    }`,
+		`//! Needed to remove these lines in order to make it work on node.js
+    // if (typeof input === 'undefined') {
+    //     input = new URL('${buildConfig.wasm.name}_bg.wasm', import.meta.url);
+    // }`
+	);
+
+fs.writeFileSync(jsOutFile, modifiedScript, "utf-8");
