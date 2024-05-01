14030c3191846aee33cf212a8736c36c6ff6c9f4
Fineshop Design <fineshopdesign@gmail.com>
2024-05-01 14:56:28 +0530

    fix: use esbuild for building modules to fix errors such as `ERR_MODULE_NOT_FOUND`, `ERR_UNSUPPORTED_DIR_IMPORT`, etc. [bump]

--- packages/png/scripts/build.ts
+++ packages/png/scripts/build.ts
@@ -1,9 +1,107 @@
-import cp from "node:child_process";
-import path from "node:path";
+/* eslint-disable no-console */
+
+/**
+ * This script is heavily inspired by `built.ts` used in @kaze-style/react.
+ * https://github.com/taishinaritomi/kaze-style/blob/main/scripts/build.ts
+ * MIT License
+ * Copyright (c) 2022 Taishi Naritomi
+ */
+
+import cp from "child_process";
+import path from "path";
 import fs from "fs-extra";
+import {
+	build as esbuild,
+	type Plugin,
+	type PluginBuild,
+	type BuildOptions
+} from "esbuild";
+import * as glob from "glob";
 import clc from "console-log-colors";
 
-import buildConfig from "./config";
+/**
+ * This plugin is inspired by the following.
+ * https://github.com/evanw/esbuild/issues/622#issuecomment-769462611
+ */
+const addExtension = (
+	fileMap: { from: string; to: string }[] = [{ from: ".ts", to: ".js" }]
+): Plugin => ({
+	name: "add-extension",
+	setup(build: PluginBuild) {
+		// eslint-disable-next-line consistent-return
+		build.onResolve({ filter: /.*/ }, (args) => {
+			if (args.importer) {
+				const p = path.join(args.resolveDir, args.path);
+
+				let importPath = "";
+
+				fileMap.forEach((config) => {
+					let formPath = `${p}${config.from}`;
+					if (fs.existsSync(formPath)) {
+						importPath = args.path + config.to;
+					} else {
+						formPath = path.join(
+							args.resolveDir,
+							args.path,
+							`index${config.from}`
+						);
+						if (fs.existsSync(formPath)) {
+							importPath = `${args.path}/index${config.to}`;
+						}
+					}
+				});
+
+				return { path: importPath, external: true };
+			}
+		});
+	}
+});
+
+// Entrypoints for ESM
+const esmEntryPoints = glob.sync("./src/**/*.{ts,js}", {
+	ignore: ["**/*.d.ts", "./src/**/*.test.ts"]
+});
+
+// Entrypoints for CJS
+const cjsEntryPoints = glob.sync("./src/**/*.{ts,js}", {
+	ignore: [
+		"**/*.d.ts",
+		"./src/**/*.test.ts",
+		"src/workers/**/*",
+		"src/next/**/*"
+	]
+});
+
+// Common build options for both CJS and ESM
+const commonOptions: BuildOptions = {
+	logLevel: "info",
+	platform: "node"
+};
+
+// Build options for ESM
+const esmOptions: BuildOptions = {
+	...commonOptions,
+	entryPoints: esmEntryPoints,
+	bundle: true,
+	outbase: "./src",
+	outdir: "./dist/esm",
+	format: "esm",
+	plugins: [
+		addExtension([
+			{ from: ".ts", to: ".js" },
+			{ from: ".js", to: ".js" }
+		])
+	]
+};
+
+// Build options for CJS
+const cjsOptions: BuildOptions = {
+	...commonOptions,
+	entryPoints: cjsEntryPoints,
+	outbase: "./src",
+	outdir: "./dist/cjs",
+	format: "cjs"
+};
 
 const startedOn = Date.now();
 
@@ -11,79 +109,48 @@ const startedOn = Date.now();
 console.log("⚡ Cleaning dist directory...");
 fs.rmSync("dist", { recursive: true, force: true });
 
-// Delete unnecessary files from output
-[
-	"package.json",
-	"LICENSE",
-	"LICENSE.md",
-	"README",
-	"README.md",
-	".gitignore"
-].forEach((file) =>
-	fs.rmSync(path.join(buildConfig.wasm.out, file), { force: true })
-);
-
-// Make changes to out js to make it compatible
-// for different environments
-const jsOutFile = path.join(
-	buildConfig.wasm.out,
-	`${buildConfig.wasm.name}.js`
-);
-
-const originalScript = fs.readFileSync(jsOutFile, "utf-8");
-
-const modifiedScript = originalScript
-	.replace(
-		`if (!(module instanceof WebAssembly.Module)) {
-        module = new WebAssembly.Module(module);
-    }`,
-		`//! Needed to remove these lines in order to make it work on next.js
-    // if (!(module instanceof WebAssembly.Module)) {
-    //     module = new WebAssembly.Module(module);
-    // }`
-	)
-	.replace(
-		`if (typeof input === 'undefined') {
-        input = new URL('${buildConfig.wasm.name}_bg.wasm', import.meta.url);
-    }`,
-		`//! Needed to remove these lines in order to make it work on node.js
-    // if (typeof input === 'undefined') {
-    //     input = new URL('${buildConfig.wasm.name}_bg.wasm', import.meta.url);
-    // }`
-	);
-
-fs.writeFileSync(jsOutFile, modifiedScript, "utf-8");
-
 // Build ESM
 console.log("⚡ Building ESM version...");
-cp.execSync(
-	["pnpm tsc", "pnpm copyfiles -u 1 ./src/lib/*.wasm ./dist/esm/"].join(" && "),
-	{ stdio: "inherit" }
-);
+await esbuild(esmOptions).catch(console.error);
 
-// Build CommonJS
+// Build CJS
 console.log("⚡ Building CJS version...");
-cp.execSync(
-	[
-		"pnpm tsc -p ./tsconfig.cjs.json",
-		"pnpm copyfiles -u 1 ./src/lib/*.wasm ./dist/cjs/"
-	].join(" && "),
-	{ stdio: "inherit" }
+await esbuild(cjsOptions).catch(console.error);
+
+// Build typings
+console.log("⚡ Building Types...");
+cp.execSync(`tsc --project tsconfig.dts.json --outDir dist/dts`, {
+	stdio: "inherit"
+});
+
+// Copy assets
+console.log("⚡ Copying Assets...");
+glob.sync("./src/**/*.{wasm,bin}").forEach((filepath) => {
+	const destinations = ["./dist/esm", "./dist/cjs"].map((directory) =>
+		path.join(directory, filepath.replace(/^src\//, ""))
+	);
+	console.log("");
+	destinations.forEach((destination) => {
+		console.log(`  ${clc.white(filepath)} => ${clc.white(destination)}`);
+		fs.copyFileSync(filepath, destination);
+	});
+	console.log("");
+});
+
+// Write package.json for CJS
+console.log("⚡ Writing package.json files...");
+const cjsPackageJson = { type: "commonjs" };
+
+["./dist/cjs/package.json", "./dist/dts/package.json"].forEach(
+	(destination) => {
+		fs.writeJSONSync(destination, cjsPackageJson, {
+			encoding: "utf8"
+		});
+	}
 );
 
-// Build Types
-console.log("⚡ Building Types...");
-cp.execSync("pnpm tsc -p ./tsconfig.dts.json", { stdio: "inherit" });
-
-// Write package.json
-console.log("⚡ Writing package.json files...");
-const cjsPackageJSON = { type: "commonjs" };
-
-fs.writeJSONSync("dist/cjs/package.json", cjsPackageJSON);
-fs.writeJSONSync("dist/dts/package.json", cjsPackageJSON);
-
+// Print time taken
 const finishedOn = Date.now();
-
 console.log(
 	clc.green(`⚡ Took ${((finishedOn - startedOn) / 1000).toFixed(2)} s`)
 );
