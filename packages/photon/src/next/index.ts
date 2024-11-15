fd21e9db3d9d38fa10892aea179030ef32676c70
Fineshop Design <fineshopdesign@gmail.com>
2024-11-15 19:19:51 +0530

    fix(photon): using deprecated parameters for initialization function, pass a single object instead. [bump]

--- packages/photon/src/next/index.ts
+++ packages/photon/src/next/index.ts
@@ -2,7 +2,7 @@ import '@cf-wasm/internals/polyfills/image-data';
 import initAsync, { initSync } from '../lib/photon_rs';
 import photonWasmModule from '../lib/photon_rs_bg.wasm?module';
 
-initSync(photonWasmModule);
+initSync({ module: photonWasmModule });
 
 export { initAsync, photonWasmModule };
 export * from '../lib/photon_rs';
